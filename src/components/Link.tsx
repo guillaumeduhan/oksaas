5fd6e0c4e39c7da747016c3dc32187eeb5127b7c
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2025-12-02 11:31:47 +0100

    commit

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -122,7 +122,7 @@ export default function Link(props: Props) {
                 Featured
               </div>
             )}
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className={`${is_featured
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`${is_featured
               ? "text-kiwi-700"
               : "text-neutral-400 dark:text-neutral-200"
               }`}><path d="M15 3h6v6" /><path d="M10 14 21 3" /><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" /></svg>
